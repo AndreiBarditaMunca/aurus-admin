4ec052fec5c6eaffb34583c21dbefa72fb0a2a36
Bardita Andrei <andreib@wolfdigitalmedia.ro>
2022-06-09 14:21:14 +0300

    Installed pg, aws-S3 upload provider and updated the config files

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'aurus'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', 'admin'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "aurus"),
+      password: env("DATABASE_PASSWORD", "aurus.147"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
+    useNullAsDefault: true,
   },
 });
