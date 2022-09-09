85865cdf065afd9acc6e6d13193e0d9abd90b6ca
Bardita Andrei <bmihaiandrei@gmail.com>
2022-09-09 14:57:37 +0300

    added internationalization for article collection type

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "aurus"),
-      user: env("DATABASE_USERNAME", "admin"),
+      user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "admin"),
       ssl: env.bool("DATABASE_SSL", false),
     },
