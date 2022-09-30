78c6d6e70fc8394b19a4e92af65de39af1ed3ff7
Bardita Andrei <bmihaiandrei@gmail.com>
2022-09-30 11:08:56 +0300

    changed tutorial difficulty structure to relation

---
+++ src/api/tutorial-difficulty/controllers/tutorial-difficulty.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tutorial-difficulty controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tutorial-difficulty.tutorial-difficulty');
---
+++ src/api/tutorial-difficulty/services/tutorial-difficulty.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tutorial-difficulty service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tutorial-difficulty.tutorial-difficulty');
